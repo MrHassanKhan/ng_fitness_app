7bcfa2b9d24c7ea1d01ec8fafb82fc5c11de5058
Muhammad Hassan <muhammadhsn09@gmail.com>
2019-06-19 16:05:17 +0500

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,14 @@
 
 export const environment = {
   production: false,
-  firebase: {
-    apiKey: 'AIzaSyAAbYYaOQ8649t3lEgjnxaUNkcY1-b86z0',
-    authDomain: 'ng-fitness-tracker-5999c.firebaseapp.com',
-    databaseURL: 'https://ng-fitness-tracker-5999c.firebaseio.com',
-    projectId: 'ng-fitness-tracker-5999c',
-    storageBucket: 'ng-fitness-tracker-5999c.appspot.com',
-    messagingSenderId: '1082243743110'
-  }
+//   firebase: {
+//     apiKey: 'AIzaSyAAbYYaOQ8649t3lEgjnxaUNkcY1-b86z0',
+//     authDomain: 'ng-fitness-tracker-5999c.firebaseapp.com',
+//     databaseURL: 'https://ng-fitness-tracker-5999c.firebaseio.com',
+//     projectId: 'ng-fitness-tracker-5999c',
+//     storageBucket: 'ng-fitness-tracker-5999c.appspot.com',
+//     messagingSenderId: '1082243743110'
+//   }
 };
 
 /*
